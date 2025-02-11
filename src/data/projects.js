90cfe43ef92ccb309f3807eb0e695117958278ef
AlvaroCanovasP <alvarocanovasp@gmail.com>
2025-02-11 15:45:35 +0100

    Restore

---
+++ src/data/projects.js
@@ -0,0 +1,128 @@
+export const projects = [
+  {
+    title: "Manifesto UOC",
+    director: "Ester G. Mera",
+    directorUrl: "https://estergmera.com/",
+    producer: "We Are CP",
+    producerUrl: "https://www.wearecp.es/",
+    image: "src/img/Still_manifesto_UOC.webp",
+    thumbnail: "src/img/gifs/Loop_Manifesto_UOC_Clip.webm",
+    videoId: "1030260797"
+  },
+  {
+    title: "UOC #02",
+    director: "Ester G. Mera",
+    directorUrl: "https://estergmera.com/",
+    producer: "We Are CP",
+    producerUrl: "https://www.wearecp.es/ ",
+    image: "src/img/Still_Universidad_UOC.webp",
+    thumbnail: "src/img/gifs/Loop_Universidad_UOC_Clip.webm",
+    videoId: "1047977263"
+  },
+  {
+    title: "Mal de Ojo",
+    director: "Ester G. Mera",
+    directorUrl: "https://estergmera.com/",
+    producer: "Topo Colectivo",
+    producerUrl: "https://www.topocolectivo.com/",
+    image: "src/img/Still_MaldeOjo.webp",
+    thumbnail: "src/img/gifs/Loop_MaldeOjo_Anaju_Clip.webm",
+    videoId: "883179965"
+  },
+  {
+    title: "Kassandra - Quevedo",
+    directors: [
+      {
+        name: "Poul Nissen",
+        url: "https://www.instagram.com/poulnissen/"
+      },
+      {
+        name: "Tomás Conterio",
+        url: "https://www.instagram.com/tocontacto/"
+      }
+    ],
+    producer: "Topo Colectivo",
+    producerUrl: "https://www.topocolectivo.com/",
+    image: "src/img/Still_Kassandra.webp",
+    thumbnail: "src/img/gifs/Loop_Kassandra_Quevedo_Clip.webm",
+    videoId: "1047964179"
+  },
+  {
+    title: "¿Que voy a hacer? - Shego",
+    director: "Ester G. Mera",
+    directorUrl: "https://estergmera.com/",
+    producer: "",
+    producerUrl: "",
+    image: "src/img/Still_Quevoyahacer.webp",
+    thumbnail: "src/img/gifs/gift_qvah_h264.webm",
+    videoId: "785184730"
+  },
+  {
+    title: "La Cruz - Lia Kali",
+    directors: [
+      {
+        name: "Poul Nissen",
+        url: "https://www.instagram.com/poulnissen/"
+      },
+      {
+        name: "Tomás Conterio",
+        url: "https://www.instagram.com/tocontacto/"
+      }
+    ],
+    producer: "Topo Colectivo",
+    producerUrl: "https://www.topocolectivo.com/",
+    image: "src/img/1_47_Still_LaCruz.webp",
+    thumbnail: "src/img/gifs/Loop_LaCruz_LiaKali_Clip.webm",
+    videoId: "987110651"
+  },
+  {
+    title: "Buenisssima - Raul CLyde",
+    director: "Ester G. Mera",
+    directorUrl: "https://estergmera.com/",
+    producer: "Menta Tv",
+    producerUrl: "https://menta.tv/",
+    image: "src/img/Still_Buenisssima.webp",
+    thumbnail: "src/img/gifs/Gift_Buenisssima_h264.webm",
+    videoId: "840754974"
+  },
+  {
+    title: "Music Makers for Caixaforum",
+    director: "Ester G. Mera",
+    directorUrl: "https://estergmera.com/",
+    producer: "Vampire",
+    producerUrl: "https://wearevampire.com/",
+    image: "src/img/Still_Trailer_MusicMakers.webp",
+    thumbnail: "src/img/gifs/gift_musicmakers_h264.webm",
+    videoId: "1027232038"
+  },
+  {
+    title: "Lucky - Shego",
+    director: "Ester G. Mera",
+    directorUrl: "https://estergmera.com/",
+    producer: "",
+    producerUrl: "",
+    image: "src/img/Still_Shego.webp",
+    thumbnail: "src/img/gifs/Loop_Lucky_Shego_Clip.webm",
+    videoId: "764116724"
+  },
+  {
+    title: "Soul BlackOut",
+    director: "Ester G. Mera",
+    directorUrl: "https://estergmera.com/",
+    producer: "",
+    producerUrl: "",
+    image: "src/img/Still_SoulBlackOut.webp",
+    thumbnail: "src/img/gifs/Gift_SoulBlackOut_h264.webm",
+    videoId: "816677472"
+  },
+  {
+    title: "BLABLA",
+    director: "Ester G. Mera",
+    directorUrl: "https://estergmera.com/",
+    producer: "We Are CP",
+    producerUrl: "https://www.wearecp.es/",
+    image: "src/img/Still_Universidad_UOC.webp",
+    thumbnail: "src/img/gifs/Loop_Universidad_UOC_Clip.webm",
+    videoId: "1047977263"
+  }
+];
